b0f5d6f1f7dd69d31fa93cb5238b72f0c62d9c17
saeedjohar <saeed_johar@yahoo.com>
2017-12-14 11:48:04 +0300

    initial commit for setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sixfab_iotshield',
+    version='0.1',
+    author='Saeed Johar',
+    author_email='saeed_johar@yahoo.com',
+    description='iotshield test library',
+    license='MIT',
+    url='https://github.com/saeedjohar/sixfabiotshield/',
+    packages=find_packages()
+
+)
