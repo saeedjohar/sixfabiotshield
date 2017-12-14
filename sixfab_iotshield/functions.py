98dfe85508ec6389364945037b7161d5bffeaf3a
saeedjohar <saeed_johar@yahoo.com>
2017-12-14 13:39:12 +0300

    lux function is modified

--- sixfab_iotshield/functions.py
+++ sixfab_iotshield/functions.py
@@ -1,2 +1,7 @@
+from .ADS1x15 import ADS1015
 def lux():
     print'This is lux'
+    adc=ADS1015(address=0x49, busnum=1)
+    rawLux=adc.read_adc(2,gain=1)
+    lux = rawLux*100/1580
+    print 'Lux : %d' %lux
