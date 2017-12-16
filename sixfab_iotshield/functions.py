8d2e0627a117fdbd8283989df32a10d79aa596e2
saeedjohar <saeed_johar@yahoo.com>
2017-12-16 15:30:48 +0300

    more functions added

--- sixfab_iotshield/functions.py
+++ sixfab_iotshield/functions.py
@@ -1,18 +1,22 @@
+from .ADS1x15 import ADS1015
+import time
+import RPi.GPIO as GPIO
+import serial
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
 
 def lux():
-    from .ADS1x15 import ADS1015
+
     #print'This is lux'
     adc=ADS1015(address=0x49, busnum=1)
     rawLux=adc.read_adc(2,gain=1)
     lux = rawLux*100/1580
     print 'Lux : %d' %lux
 
+
 def relay():
-    import time
-    import RPi.GPIO as GPIO
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(False)
+
 
     relay1 = 21
     relay2 = 26
@@ -37,3 +41,76 @@ def relay():
     GPIO.output(relay2, False)
     print "RELAY2: OFF"
     time.sleep(2)
+
+def checkStatus():
+    statusPin = 13
+    power_key = 12
+
+    GPIO.setup(statusPin, GPIO.IN)
+    GPIO.setup(power_key, GPIO.OUT)
+
+    def turnModuleOn():
+        GPIO.output(power_key, 1)
+        time.sleep(2)
+        GPIO.output(power_key, 0)
+        time.sleep(2)
+
+    def checkStatus():
+        status = GPIO.input(statusPin)
+        if status == 0:
+            print "Module turned OFF"
+        else:
+            print "Module turned ON"
+        time.sleep(0.1)
+        return status
+
+    status = checkStatus()
+
+    if status == 0:
+        turnModuleOn()
+
+def connectivity():
+    port = "/dev/ttyS0"
+    ser = serial.Serial(port, baudrate=115200, timeout=0.5)
+
+    print "---------Checking SIM and Connection---------"
+    # time.sleep(0.5)
+    ser.write('AT\r')
+    print ser.readline()
+    print ser.readline()
+
+    time.sleep(1)
+    ser.write('AT+CPIN?\r')
+    print ser.readline()
+    print ser.readline()
+    ser.close()
+    time.sleep(0.1)
+
+
+def adc():
+    print "----------Printing ADS1015 values----------"
+    adcValues = [0] * 4
+    for i in range(0, 4):
+        adcValues[i] = adc.read_adc(i, gain=1)
+        print "ADC Ch%d : %d" % (i, adcValues[i])
+
+def opto():
+    opto1 = 20
+    opto2 = 19
+
+    GPIO.setup(opto1, GPIO.IN)
+    GPIO.setup(opto2, GPIO.IN)
+    print "----------OPTO TEST----------"
+
+    readOpto1 = GPIO.input(opto1)
+    readOpto2 = GPIO.input(opto2)
+
+    if (readOpto1 == 1):
+        print "INPUT1: LOW"
+    else:
+        print "INPUT1: HIGH"
+
+    if (readOpto2 == 1):
+        print "INPUT2: LOW"
+    else:
+        print "INPUT2: HIGH"
