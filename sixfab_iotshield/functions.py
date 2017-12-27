b1f2b9cda9af2b1ce5c9cf5e06c198f890e8b5cc
saeedjohar <saeed_johar@yahoo.com>
2017-12-27 13:07:59 +0300

    without connectivity test

--- sixfab_iotshield/functions.py
+++ sixfab_iotshield/functions.py
@@ -1,55 +1,50 @@
+#import libraries
+from enum import Enum
 from .ADS1x15 import ADS1015
 import time
 import RPi.GPIO as GPIO
 import serial
+
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 ##-----setting pin------
 statusPin = 13
 power_key = 12
-relay1 = 21
-relay2 = 26
-opto1 = 20
-opto2 = 19
-adc=ADS1015(address=0x49, busnum=1)
+
+port = "/dev/ttyS0"
+
+GPIO.setup(statusPin, GPIO.IN)
+GPIO.setup(power_key, GPIO.OUT)
+
+class switchStatus(Enum):
+    OFF = False
+    ON = True
+
+class relayNumber(Enum):
+    relay1 = 21
+    relay2 = 26
+
+class opto(Enum):
+    opto1 = 20
+    opto2 = 19
 
 
 
 def lux():
-    #print'This is lux'
-
+    adc = ADS1015(address=0x49, busnum=1)
     rawLux=adc.read_adc(2,gain=1)
     lux = rawLux*100/1580
-    print 'Lux : %d' %lux
+    return lux
 
-
-def relay():
-    GPIO.setup(relay1, GPIO.OUT)
-    GPIO.setup(relay2, GPIO.OUT)
-
-    print "----------RELAY TEST----------"
-
-    GPIO.output(relay1, True)
-    print "RELAY1: ON"
+def relay(relayNumber,switchStatus):
+    GPIO.setup(relayNumber, GPIO.OUT)
+    GPIO.output(relayNumber, switchStatus)
+    #print "RELAY1: ON"
     time.sleep(1)
 
-    GPIO.output(relay1, False)
-    print "RELAY1: OFF"
-    time.sleep(2);
-
-    GPIO.output(relay2, True)
-    print "RELAY2: ON"
-    time.sleep(1)
-
-    GPIO.output(relay2, False)
-    print "RELAY2: OFF"
-    time.sleep(2)
 
 def status():
-    GPIO.setup(statusPin, GPIO.IN)
-    GPIO.setup(power_key, GPIO.OUT)
-
     def turnModuleOn():
         GPIO.output(power_key, 1)
         time.sleep(2)
@@ -70,45 +65,29 @@ def status():
     if status == 0:
         turnModuleOn()
 
-def connectivity():
-    port = "/dev/ttyS0"
+'''
+def connectivity(string):
     ser = serial.Serial(port, baudrate=115200, timeout=0.5)
 
-    print "---------Checking SIM and Connection---------"
-    # time.sleep(0.5)
-    ser.write('AT\r')
-    print ser.readline()
-    print ser.readline()
+    ser.write(string+'\r')
+    #ser.write('AT\r')
+    ser.readline()
+    return ser.readline()
     time.sleep(1)
-    ser.write('AT+CPIN?\r')
-    print ser.readline()
-    print ser.readline()
-    ser.close()
-    time.sleep(0.1)
+'''
 
 def adc(i):
-    #print "----------Printing ADS1015 values----------"
+    adc = ADS1015(address=0x49, busnum=1)
     adcValues = [0] * 4
-
-    #for i in range(0, 4):
-    #   adcValues[i] = adc.read_adc(i, gain=1)
-    #   print "ADC Ch%d : %d" % (i, adcValues[i])
-
     adcValues[i] = adc.read_adc(i, gain=1)
+    return adcValues[i]
 
-def opto():
-    GPIO.setup(opto1, GPIO.IN)
-    GPIO.setup(opto2, GPIO.IN)
-    print "----------OPTO TEST----------"
 
-    readOpto1 = GPIO.input(opto1)
-    readOpto2 = GPIO.input(opto2)
-    if (readOpto1 == 1):
-        print "INPUT1: LOW"
-    else:
-        print "INPUT1: HIGH"
 
-    if (readOpto2 == 1):
-        print "INPUT2: LOW"
-    else:
-        print "INPUT2: HIGH"
+def optoRead(opto):
+    GPIO.setup(opto, GPIO.IN)
+    optoReading = GPIO.input(opto)
+    if (optoReading==optoRead.HIGH):
+        return "LOW"
+    elif():
+        print "HIGH"
